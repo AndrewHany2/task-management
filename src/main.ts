65a0dfbed11f205ae01c2da982a84394416324b5
Andrew Hany <andrewhany260@gmail.com>
2024-01-23 17:43:28 +0200

    for prodction upload

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as config from 'config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const server = config.get('server');
+  if(process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: server.origin });
+  }
   const port = process.env.PORT || server.port;
   await app.listen(port);
 }
